10b14b58797e42a5ee0958b8cf94311c9e5ca84b
Andy Atkinson <andyatkinson@gmail.com>
2019-11-12 22:38:10 -0600

    Add Trips table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_151637) do
+ActiveRecord::Schema.define(version: 2019_11_12_165848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,19 @@ ActiveRecord::Schema.define(version: 2019_11_11_151637) do
     t.integer "end_location_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["rider_id"], name: "index_trip_requests_on_rider_id"
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.integer "trip_request_id", null: false
+    t.integer "driver_id", null: false
+    t.datetime "completed_at"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["driver_id"], name: "index_trips_on_driver_id"
+    t.index ["rating"], name: "index_trips_on_rating"
+    t.index ["trip_request_id"], name: "index_trips_on_trip_request_id"
   end
 
   create_table "users", force: :cascade do |t|
