cf3cd67c81ef5788c351eb6fe29ec68ee758cb74
Andy Atkinson <andyatkinson@gmail.com>
2020-05-14 09:35:42 -0500

    Add a test to see a single trip

--- test/controllers/api/trips_controller_test.rb
+++ test/controllers/api/trips_controller_test.rb
@@ -24,4 +24,16 @@ class Api::TripsControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
     assert_equal 1, response.parsed_body.size
   end
+
+  test "show a single trip" do
+    get api_trip_url(trip)
+    assert_response 200
+    assert_equal trip.id, response.parsed_body['id']
+  end
+
+  private
+
+  def trip
+    @trip ||= trips(:completed_trip)
+  end
 end
