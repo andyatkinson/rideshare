43332d90e9c756af36838c444548c75979c186fe
Andy Atkinson <andyatkinson@gmail.com>
2020-05-18 12:38:57 -0500

    Fix rating formatting
    This doesn't really make sense as a percentage, and should just be a
    float rounded to 2 places

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,6 +1,7 @@
 class Driver < User
   has_many :trips
 
+  # X out of 5, with 1 to 5 options selected by Riders
   def average_rating
     trips.average(:rating)
   end
--- app/serializers/driver_serializer.rb
+++ app/serializers/driver_serializer.rb
@@ -1,14 +1,9 @@
 class DriverSerializer
   include FastJsonapi::ObjectSerializer
 
-  extend ActionView::Helpers::NumberHelper
-
   attribute :display_name
 
   attribute :average_rating do |driver|
-    number_to_percentage(
-      driver.average_rating,
-      precision: 2
-    )
+    driver.average_rating.round(2)
   end
 end
