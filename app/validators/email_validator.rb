fc2ef37bfbb5d8809f7636960b1d4af2ff10eecf
Andy Atkinson <andyatkinson@gmail.com>
2021-07-02 12:22:12 -0500

    Update Rails
    Address a deprecation warning about adding model errors

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,8 +1,9 @@
 # https://guides.rubyonrails.org/active_record_validations.html#custom-validators
 class EmailValidator < ActiveModel::EachValidator
+
   def validate_each(record, attribute, value)
     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
+      record.errors.add(attribute, (options[:message] || "is not an email"))
     end
   end
 end
