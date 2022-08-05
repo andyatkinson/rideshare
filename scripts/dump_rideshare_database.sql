9d99741547eeb048095b7062738b5db7387a1a2a
Andy Atkinson <andyatkinson@gmail.com>
2022-08-05 11:03:35 -0500

    Improved naming and adding more status output

--- scripts/dump_rideshare_database.sql
+++
@@ -1,9 +0,0 @@
-# run pg_dump for users table
-# https://www.postgresql.org/docs/current/app-pgdump.html
-#
-# -s/--schema-only
-# -O/--no-owner
-# -x/--no-privileges/--no-acl
-# -f/--file send output to a file
-
-pg_dump --dbname rideshare_development --no-owner --no-privileges --file tmp/dump_file.sql
