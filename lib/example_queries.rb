929fc27844fc34b368f2a4704e6f29e975b778a8
Andy Atkinson <andyatkinson@gmail.com>
2022-10-07 13:03:02 -0500

    Update rails (bundle update rails)
    Add more example queries

--- lib/example_queries.rb
+++ lib/example_queries.rb
@@ -7,3 +7,6 @@ Trip.completed.
   includes(:driver, {trip_request: :rider}).
   joins(trip_request: :rider).
   where(users: {id: rider_id})
+
+# Trip requests for a rider
+TripRequest.where(rider_id: 1).first
