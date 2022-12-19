22408b172518336bed4da70e74610770f238725c
Andy Atkinson <andyatkinson@gmail.com>
2022-12-19 11:44:02 -0600

    Add TripCreator service
    Add test for service class
    
    Invoke it and test it

--- scripts/simulate_app_traffic.rb
+++ scripts/simulate_app_traffic.rb
@@ -15,6 +15,7 @@ require 'json'
 # 1. Driver completes trip
 
 
+# 1. create trip request
 url = 'http://localhost:3000/api/trip_requests'
 request_body = {
   trip_request: {
@@ -29,3 +30,18 @@ request_headers = {
 response = Faraday.post(url, request_body, request_headers)
 resp = JSON.parse(response.body, symbolize_names: true)
 puts resp
+
+# 1. poll for trip request status, until trip exists
+if resp && trip_request_id = resp['trip_request_id'].to_i
+  url = "http://localhost:3000/api/trip_requests/#{trip_request_id}"
+  request_headers = {
+    'Accept' => 'application/json'
+  }
+  response = Faraday.get(url, nil, request_headers)
+  resp = JSON.parse(response.body, symbolize_names: true)
+  puts resp
+  trip_id = resp['trip_id']
+  puts "trip_id: #{trip_id}"
+end
+
+# 1. poll for trip status, until trip is completed
