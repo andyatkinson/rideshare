00762e99b9d86f1d49cd764277f982800ab90239
Andy Atkinson <andyatkinson@gmail.com>
2023-03-14 16:38:54 -0500

    Retire and unretire
    Adding SQL inside a transtion to:
    
    - Detach, and retire the DEFAULT partition
    - Unretire, and ATTACH the DEFAULT partition

--- app/lib/pgslice_helper.rb
+++ app/lib/pgslice_helper.rb
@@ -5,6 +5,9 @@
 # - prep: pgslice prep
 # - bin/rails runner "PgsliceHelper.new.add_partitions(table_name: 'trip_positions', past: 0, future: 3)"
 #
+# Prep:
+# Retire default
+# - bin/rails runner "PgsliceHelper.new.retire_default_partition(table_name: 'trip_positions')"
 #
 # To test app compatibility:
 # - Make sure latest changes from dev DB are applied: `bin/rails db:test:prepare`
@@ -34,10 +37,50 @@ class PgsliceHelper
   def unswap
   end
 
-  def retire_default_partition
+  # default partitions cannot be detached concurrently
+  # "ERROR:  cannot detach partitions concurrently when a default partition exists"
+  def retire_default_partition(table_name:, dry_run: false)
+    table_name = "#{table_name}_intermediate" # assumes intermediate table
+    partition_name = "#{table_name}_default"
+    retired_name = "#{partition_name}_retired"
+
+    sql = %(
+      BEGIN;
+
+      ALTER TABLE #{table_name} \
+      DETACH PARTITION #{partition_name};
+
+      ALTER TABLE #{partition_name}
+      RENAME TO #{retired_name}
+    ).squish
+
+    cmd = %(psql $PGSLICE_URL -c '#{sql}')
+    log("detaching and retiring dry_run=#{dry_run} cmd=#{cmd}")
+    log("cmd=#{cmd}")
+    system(cmd) unless dry_run
   end
 
-  def unretire_default_partition
+  def unretire_default_partition(table_name:, dry_run: false)
+    table_name = "#{table_name}_intermediate" # assumes intermediate table
+    partition_name = "#{table_name}_default"
+    retired_name = "#{partition_name}_retired"
+
+    sql = %(
+      BEGIN;
+
+      ALTER TABLE #{retired_name}
+      RENAME TO #{partition_name};
+
+      ALTER TABLE #{table_name}
+      ATTACH PARTITION #{partition_name}
+      DEFAULT;
+
+      COMMIT;
+    ).squish
+    cmd = %(psql $PGSLICE_URL -c '#{sql}')
+    log("unretiring and attaching. dry_run=#{dry_run}")
+    log("cmd=#{cmd}")
+    system(cmd) unless dry_run
   end
 
   private
