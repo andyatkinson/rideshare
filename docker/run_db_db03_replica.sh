7bf417458ed3a76b554b988c4f45f142f2d318dc
Andy Atkinson <andyatkinson@gmail.com>
2023-09-27 13:45:27 -0500

    Add db03 script

---
+++ docker/run_db_db03_replica.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# db03 uses Logical Replication
+docker run \
+  --name db03 \
+  --volume ${PWD}/postgres-docker/db03:/var/lib/postgresql/data \
+  --publish 54323:5432 \
+  --env POSTGRES_USER=postgres \
+  --env POSTGRES_PASSWORD=postgres \
+  --net=rideshare-net \
+  --detach postgres:16
