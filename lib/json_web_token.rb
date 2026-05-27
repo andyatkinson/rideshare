9c475be7926498575a51cf0596b1c36455e031ac
Andrew Atkinson <andyatkinson@gmail.com>
2026-05-26 22:15:34 -0500

    Updates

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,9 +1,11 @@
 class JsonWebToken
-  SECRET_KEY = Rails.application.credentials.secret_key_base.to_s
+  SECRET_KEY = ENV.fetch("JWT_SECRET") do
+    Rails.application.credentials.secret_key_base.to_s
+  end
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+    JWT.encode(payload, SECRET_KEY, "HS256")
   end
 
   def self.decode(token)
