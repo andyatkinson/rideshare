49db4362e5c04050839d437bdae07e7d4be89950
Andrew Atkinson <andyatkinson@gmail.com>
2026-05-26 22:27:41 -0500

    [Maintenance] Update gem dependencies  (#261)
    * Updates
    
    * Updates
    
    * Change version of minitest
    
    * Updates
    
    * Updates

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,9 +1,9 @@
 class JsonWebToken
-  SECRET_KEY = Rails.application.credentials.secret_key_base.to_s
+  SECRET_KEY = Rails.application.secret_key_base
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+    JWT.encode(payload, SECRET_KEY, "HS256")
   end
 
   def self.decode(token)
