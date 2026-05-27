7e265b93ce20f30bf175f7edb381e1b530e2c17f
Andrew Atkinson <andyatkinson@gmail.com>
2026-05-26 23:31:28 -0500

    Updates

---
+++ docker/setup_docker_workshop.sh
@@ -0,0 +1,8 @@
+docker network inspect rideshare-net >/dev/null 2>&1 || docker network create rideshare-net
+
+docker rm -f db01 >/dev/null 2>&1 && sh ./docker/run_db_db01_primary.sh
+docker rm -f db02 >/dev/null 2>&1 && sh ./docker/run_db_db02_replica.sh
+
+docker ps
+
+export DOCKER_CLI_HINTS=false
